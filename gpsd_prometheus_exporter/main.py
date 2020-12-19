04c235bf002baeccf377599ee1c0c14363b536bf
Mark S <github@sharpley.xyz@users.noreply.github.com>
2020-12-19 13:27:26 +0000

    [ADD] version management
    Signed-off-by: Mark S <github@sharpley.xyz@users.noreply.github.com>

--- gpsd_prometheus_exporter/main.py
+++ gpsd_prometheus_exporter/main.py
@@ -8,9 +8,10 @@ from prometheus_client import start_http_server, Gauge, Histogram
 
 LONGITUDE = Gauge("longitude", "longitude measured")
 LATITUDE = Gauge("latitude", "latitude measured")
-SPEED = Gauge("speed", "Current speed in knots")
+SPEED = Gauge("speed", "Current speed")
 ALTITUDE = Gauge("altitude", "Current Altitude in metres")
 SAT_COUNT = Gauge("satcount", "Number of satellites")
+
 
 def gps_connect():
     gpsd.connect()
@@ -18,17 +19,33 @@ def gps_connect():
     data = gpsd.get_current()
     return data
 
-def get_gpsd_data(packet):
+
+def get_gpsd_data(unit, packet):
     """Set Prom Metrics"""
     LONGITUDE.set(packet.lon)
     LATITUDE.set(packet.lat)
-    SPEED.set(mps_to_knots(packet.hspeed))
+    SPEED.set(speed_conversion(unit, packet.hspeed))
     ALTITUDE.set(packet.alt)
     SAT_COUNT.set(str(packet.sats))
 
 
-def mps_to_knots(mps):
-    return 1.94384 * mps
+def set_speed_units(unit):
+    if unit == "mps" or unit == "knots" or unit == "mph" or unit == "kph":
+        pass
+    else:
+        logging.info(f"I don't recognise {unit}. Use --help for more information")
+        exit(1)
+
+
+def speed_conversion(unit, mps):
+    if unit == "knots":
+        return 1.94384 * mps
+    elif unit == "mph":
+        return 2.237 * mps
+    elif unit == "kph":
+        return 3.6 * mps
+    else:
+        return mps
 
 
 def make_json():
@@ -40,6 +57,7 @@ def make_json():
     sensor_data["altitude"] = ALTITUDE.collect()[0].samples[0].value
     sensor_data["satcount"] = SAT_COUNT.collect()[0].samples[0].value
     return sensor_data
+
 
 @click.command()
 @click.option(
@@ -57,24 +75,35 @@ def make_json():
     help="Specify alternate port [default: 8000]",
 )
 @click.option(
+    "--speedunit",
+    "-s",
+    default="mps",
+    type=str,
+    help="""Units for speed. Defaults to mps (metres per second).
+    Could set knots, mph (mile per hour) or kph (kilometers per hour)""",
+)
+@click.option(
     "--debug",
     "-d",
     is_flag=True,
     help="Turns on more verbose logging, prints output [default: False]",
 )
-def main(bind, port, debug):
+def main(bind, port, debug, speedunit):
 
     logging.basicConfig(
         format="%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s",
         level=logging.INFO,
         datefmt="%Y-%m-%d %H:%M:%S",
     )
+    set_speed_units(speedunit)
     logging.info(f"Listening on http://{bind}:{port}")
+    logging.info(f"Reporting speed in {speedunit}")
+
     start_http_server(addr=bind, port=port)
 
     while True:
         data = gps_connect()
-        get_gpsd_data(data)
+        get_gpsd_data(speedunit, data)
         time.sleep(1)
         if debug:
             logging.info(f"Sensor data: {make_json()}")
